d7d3a869c4a65626058fe154e099ece4757487ed
riyadh9292 <mrriyadh66@gmail.com>
2021-10-31 22:17:34 +0600

    cant

--- index.js
+++ index.js
@@ -98,6 +98,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
